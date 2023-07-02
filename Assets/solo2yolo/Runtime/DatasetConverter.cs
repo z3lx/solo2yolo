9b16d637f8d0500305474f2b0a5ca720ce46f277
z3lx <57017122+z3lx@users.noreply.github.com>
2023-07-02 12:49:12 -0400

    Add basic editor window and runtime skeleton for dataset converter

---
+++ Assets/solo2yolo/Runtime/DatasetConverter.cs
@@ -0,0 +1,10 @@
+namespace z3lx.solo2yolo
+{
+    public static class DatasetConverter
+    {
+        public static void Convert(string soloPath, string yoloPath, ComputerVisionTask task, bool log = false)
+        {
+
+        }
+    }
+}
