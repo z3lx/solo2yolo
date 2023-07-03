e49a2a725e3f1e20f2b6a4b850453612cd8c1bc6
z3lx <z3lx@pm.me>
2023-07-03 19:05:57 -0400

    Update custom editor window

--- Assets/solo2yolo/Editor/DatasetConverterWindow.cs
+++ Assets/solo2yolo/Editor/DatasetConverterWindow.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,21 +7,81 @@ namespace z3lx.solo2yolo
 {
     public class DatasetConverterWindow : EditorWindow
     {
+        private static readonly string _windowTitle = "Converter";
+        private Texture2D _folderIcon;
+
+        private Vector2 _margins = new Vector2(4, 4);
+        private float _lineHeight = 24;
+        private float _buttonHeight = 20;
+        private float _labelWidth = 144;
+        private float _buttonWidth = 96;
+        private float _iconWidth = 24;
+
         private string _soloPath;
         private string _yoloPath;
-        private ComputerVisionTask _task = ComputerVisionTask.Detect;
+        private ComputerVisionTask _task;
 
         [MenuItem("Tools/solo2yolo Converter")]
         private static void ShowWindow()
         {
-            GetWindow(typeof(DatasetConverterWindow));
+            DatasetConverterWindow window = GetWindow<DatasetConverterWindow>();
+            window.Initialize();
+        }
+
+        private void Initialize()
+        {
+            titleContent = new GUIContent(_windowTitle);
+            minSize = new Vector2((2 * _margins.y) + _labelWidth + _iconWidth + 432 + _buttonWidth, (2 * _margins.y) + (3 * _lineHeight) + _buttonHeight);
+
+            string defaultPath = Path.Combine(Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName, "LocalLow", PlayerSettings.companyName, PlayerSettings.productName);
+            if (Directory.Exists(defaultPath))
+            {
+                _soloPath = Path.Combine(defaultPath, "solo");
+                _yoloPath = defaultPath;
+            }
+            _task = ComputerVisionTask.Detect;
+            _folderIcon = EditorGUIUtility.FindTexture("d_FolderOpened Icon");
         }
 
         private void OnGUI()
         {
-            _soloPath = EditorGUILayout.TextField("SOLO directory", _soloPath);
-            _yoloPath = EditorGUILayout.TextField("Output directory", _yoloPath);
-            if (GUILayout.Button("Convert"))
+            Rect soloLabelRect = new Rect(_margins.x, _margins.y, _labelWidth, _lineHeight);
+            Rect soloIconRect = new Rect(_margins.x + _labelWidth, _margins.y, _iconWidth, _lineHeight);
+            Rect soloPathRect = new Rect(_margins.x + _labelWidth + _iconWidth, _margins.y, position.width - (2 * _margins.x) - _labelWidth - _iconWidth - _buttonWidth, _lineHeight);
+            Rect soloButtonRect = new Rect(position.width - _margins.y - _buttonWidth, _margins.y + + ((_lineHeight - _buttonHeight) / 2), _buttonWidth, _buttonHeight);
+
+            Rect yoloLabelRect = new Rect(_margins.x, _margins.y + _lineHeight, _labelWidth, _lineHeight);
+            Rect yoloIconRect = new Rect(_margins.x + _labelWidth, _margins.y + _lineHeight, _iconWidth, _lineHeight);
+            Rect yoloPathRect = new Rect(_margins.x + _labelWidth + _iconWidth, _margins.y + _lineHeight, position.width - (2 * _margins.x) - _labelWidth - _iconWidth - _buttonWidth, _lineHeight);
+            Rect yoloButtonRect = new Rect(position.width - _margins.y - _buttonWidth, _margins.y + _lineHeight + ((_lineHeight - _buttonHeight) / 2), _buttonWidth, _buttonHeight);
+
+            Rect yoloTaskLabelRect = new Rect(_margins.x, _margins.y + (2 * _lineHeight), _labelWidth, _lineHeight);
+            Rect yoloTaskEnumRect = new Rect(_margins.x + _labelWidth, _margins.y + (2 * _lineHeight) + ((_lineHeight - _buttonHeight) / 2), position.width - (2 * _margins.x) - _labelWidth, _buttonHeight);
+            Rect confirmButtonRect = new Rect(position.width - _margins.y - _buttonWidth, position.height - _margins.x - _buttonHeight, _buttonWidth, _buttonHeight);
+
+            GUI.Label(soloLabelRect, "SOLO directory");
+            GUI.Label(soloIconRect, _folderIcon);
+            GUI.Label(soloPathRect, _soloPath);
+            if (GUI.Button(soloButtonRect, "Choose Folder"))
+            {
+                string input = EditorUtility.OpenFolderPanel("Select SOLO directory", "", "");
+                _soloPath = string.IsNullOrEmpty(input) ? _soloPath : input;
+            }
+
+            GUI.Label(yoloLabelRect, "YOLO directory");
+            GUI.Label(yoloIconRect, _folderIcon);
+            GUI.Label(yoloPathRect, _yoloPath);
+            if (GUI.Button(yoloButtonRect, "Choose Folder"))
+            {
+                string input = EditorUtility.OpenFolderPanel("Select YOLO directory", "", "");
+                _yoloPath = string.IsNullOrEmpty(input) ? _yoloPath : input;
+            }
+
+            GUI.Label(yoloTaskLabelRect, "YOLO task");
+            EditorGUI.BeginDisabledGroup(true);
+            _task = (ComputerVisionTask)EditorGUI.EnumPopup(yoloTaskEnumRect, _task);
+            EditorGUI.EndDisabledGroup();
+            if (GUI.Button(confirmButtonRect, "Confirm"))
                 DatasetConverter.Convert(_soloPath, _yoloPath, _task);
         }
     }
