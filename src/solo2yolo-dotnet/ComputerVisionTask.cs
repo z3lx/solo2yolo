a7f2c1fbb421761e6b588641aaf3be5c395ef4f0
z3lx <57017122+z3lx@users.noreply.github.com>
2023-07-04 15:41:03 -0400

    Integrate converter runtime scripts into dotnet project

---
+++ src/solo2yolo-dotnet/ComputerVisionTask.cs
@@ -0,0 +1,10 @@
+﻿namespace z3lx.solo2yolo
+{
+    public enum ComputerVisionTask
+    {
+        Classify,
+        Detect,
+        Segment,
+        Pose
+    }
+}
---
+++ src/solo2yolo-dotnet/Json/DataModels/RgbCapture.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.Numerics;
+using z3lx.solo2yolo.Json.Converters;
+
+namespace z3lx.solo2yolo.Json.DataModels
+{
+    /// <summary>
+    /// Extends the capture class with data specific for an RGB camera sensor.
+    /// </summary>
+    [Serializable]
+    public sealed class RgbCapture : Capture
+    {
+        /// <summary>
+        /// A single file that stores sensor captured data.
+        /// </summary>
+        [JsonProperty("filename")]
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// The format of the sensor captured file.
+        /// </summary>
+        [JsonProperty("imageFormat")]
+        public string ImageFormat { get; set; }
+
+        /// <summary>
+        /// The image size in pixels (width/height).
+        /// </summary>
+        [JsonProperty("dimension")]
+        [JsonConverter(typeof(Vector2Converter))]
+        public Vector2 Dimension { get; set; }
+
+        /// <summary>
+        /// Holds the type of projection the camera used for the capture (perspective/orthographic).
+        /// </summary>
+        [JsonProperty("projection")]
+        public string Projection { get; set; }
+
+        /// <summary>
+        /// The projection matrix of the camera.
+        /// </summary>
+        [JsonProperty("matrix")]
+        public float[] Matrix { get; set; }
+    }
+}
