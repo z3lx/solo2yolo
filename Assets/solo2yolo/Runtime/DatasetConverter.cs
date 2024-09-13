edd1537a190a21d066b53c7de32fe70187edbe1c
z3lx <57017122+z3lx@users.noreply.github.com>
2024-09-13 08:53:06 -0400

    Refactor to improve readability and cross-platform support

--- Assets/solo2yolo/Runtime/DatasetConverter.cs
+++ Assets/solo2yolo/Runtime/DatasetConverter.cs
@@ -7,122 +7,106 @@ using z3lx.solo2yolo.Deserialization.DataModels;
 
 namespace z3lx.solo2yolo
 {
+    /// <summary>
+    /// Provides functionality to convert a SOLO dataset to the YOLO format.
+    /// </summary>
     public static class DatasetConverter
     {
         // TODO: Add image dimension verification
+        // TODO: Add logging capabilities
+        /// <summary>
+        /// Converts a SOLO dataset to the YOLO format.
+        /// </summary>
+        /// <param name="soloPath">The path to the SOLO dataset.</param>
+        /// <param name="outputPath">The path to store the converted YOLO dataset.</param>
+        /// <param name="task">The computer vision task conversion to perform.</param>
+        /// <param name="log">Flag indicating whether to enable logging.</param>
         public static void Convert(string soloPath, string outputPath, ComputerVisionTask task, bool log = false)
         {
-            // Validate input and output paths
-            try { ValidateAndSanitizePath(ref soloPath); }
-            catch (Exception ex) { throw new InvalidPathException($"SOLO path invalid: {ex.Message.ToLower()}"); }
+            ValidateAndSanitizePath(ref soloPath);
+            ValidateAndSanitizePath(ref outputPath);
 
-            try { ValidateAndSanitizePath(ref outputPath); }
-            catch (Exception ex) { throw new InvalidPathException($"Output path invalid: {ex.Message.ToLower()}"); }
+            string yoloPath = CreateYoloDirectory(outputPath);
 
-            // Create new YOLO directory and cache its path
-            (string parent, string images, string labels) yoloPath = CreateYoloDirectory(outputPath);
-
-            // Find and deserialize metadata
-            // TODO: Add validations
-            Metadata metadata = GetMetadata(soloPath);
+            Metadata metadata = DeserializeObjectFromFile<Metadata>(Path.Combine(soloPath, "metadata.json"));
 
             // Iterate through all sequences and frames
-            int totalFrameCount = 0; // labeled frames
+            int labelingFrameCount = 0;
             int sequenceCount = 0;
             for (int i = 0; sequenceCount < metadata.TotalSequences; i++)
             {
-                string sequencePath = $"{soloPath}/sequence.{i}";
-                if (!Directory.Exists(sequencePath)) continue;
+                string sequencePath = Path.Combine(soloPath, $"sequence.{i}");
+                if (!Directory.Exists(sequencePath))
+                    continue;
+
                 sequenceCount++;
 
                 int frameCount = 0;
-                int framesInSequence = Directory.GetFiles(sequencePath, "*.json").Length;
+                int framesInSequence = Directory.EnumerateFiles(sequencePath, "*.json").ToArray().Length;
                 for (int j = 0; frameCount < framesInSequence; j++)
                 {
-                    string frameDataPath = $"{sequencePath}/step{j}.frame_data.json";
-                    if (!File.Exists(frameDataPath)) continue;
+                    string frameDataPath = Path.Combine(sequencePath, $"step{j}.frame_data.json");
+                    if (!File.Exists(frameDataPath))
+                        continue;
+
                     frameCount++;
 
-                    // TODO: Add validations
-                    string frameDataContent = File.ReadAllText(frameDataPath);
-                    FrameData frameData = JsonConvert.DeserializeObject<FrameData>(frameDataContent);
+                    FrameData frameData = DeserializeObjectFromFile<FrameData>(frameDataPath);
 
                     // Hardcoded RGB capture
-                    RgbCapture capture = frameData.Captures[0] as RgbCapture;
-                    
-                    // Continue with the next frame if there isn't an image associated with the capture
-                    if (string.IsNullOrEmpty(capture.FileName)) continue;
+                    RgbCapture capture = frameData.Captures.OfType<RgbCapture>().FirstOrDefault();
+                    if (capture == null || string.IsNullOrEmpty(capture.FileName))
+                        continue;
 
-                    // Generate annotations for the frame depending on the task
-                    string labelingData = "";
-                    switch (task)
-                    {
-                        case ComputerVisionTask.Classify:
-                            throw new NotImplementedException();
-                        case ComputerVisionTask.Detect:
-                            // Can there be multiple of the same annotation type?
-                            BoundingBox2DAnnotation annotation = null;
-                            foreach (Annotation e in capture.Annotations)
-                            {
-                                if (e is BoundingBox2DAnnotation boundingBox2DAnnotation)
-                                {
-                                    annotation = boundingBox2DAnnotation;
-                                    break;
-                                }
-                            }
-
-                            // Continue with the next frame if there isn't an annotation of type BoundingBox2DAnnotation associated with the capture
-                            if (annotation == null) continue;
-
-                            foreach (BoundingBox2DAnnotation.Value value in annotation.Values)
-                            {
-                                int objectClassId = value.LabelId;
-                                double x = (double)(value.Origin.x + (value.Dimension.x / 2)) / capture.Dimension.x;
-                                double y = (double)(value.Origin.y + (value.Dimension.y / 2)) / capture.Dimension.y;
-                                double width = (double)value.Dimension.x / capture.Dimension.x;
-                                double height = (double)value.Dimension.y / capture.Dimension.y;
-                                labelingData += $"{objectClassId} {x} {y} {width} {height}\n";
-                            }
-                            break;
-                        case ComputerVisionTask.Segment:
-                            throw new NotImplementedException();
-                        case ComputerVisionTask.Pose:
-                            throw new NotImplementedException();
-                    }
+                    // Convert annotations for the frame depending on the task
+                    string labelingData = ConvertLabelingData(capture, task);
+                    if (string.IsNullOrEmpty(labelingData))
+                        continue;
 
                     // Write labeling data to output
-                    File.WriteAllText($"{yoloPath.labels}/{totalFrameCount:D12}.txt", labelingData);
+                    string labelingDataPath = Path.Combine(yoloPath, "labels", $"{labelingFrameCount:D12}.txt");
+                    File.WriteAllText(labelingDataPath, labelingData);
 
                     // Copy image to output
-                    File.Copy($"{sequencePath}/{capture.FileName}", $"{yoloPath.images}/{totalFrameCount:D12}.{capture.ImageFormat.ToLower()}");
+                    string sourceImagePath = Path.Combine(sequencePath, capture.FileName);
+                    string destImagePath = Path.Combine(yoloPath, "images", $"{labelingFrameCount:D12}.{capture.ImageFormat.ToLower()}");
+                    File.Copy(sourceImagePath, destImagePath);
 
-                    totalFrameCount++;
+                    labelingFrameCount++;
                 }
             }
 
-            // TODO: Add validations
-            AnnotationDefinition annotationDefinition = GetAnnotationDefinition(soloPath).Values[0];
-
-            // Generate dataset.yaml
-            string content = "";
-            content += $"path: {yoloPath.parent} # dataset root dir\n";
-            content += $"train: images # train images (relative to 'path')\n";
-            content += $"val: images # val images (relative to 'path')\n";
-            content += $"test: #test images (optional)\n";
-            content += $"\n";
-            content += $"# Classes ({annotationDefinition.Specifications.Length} classes)\n";
-            content += $"names:\n";
-            foreach (AnnotationDefinition.Specification spec in annotationDefinition.Specifications)
-                content += $"  {spec.LabelId}: {spec.labelName}\n";
-            File.WriteAllText($"{yoloPath.parent}/dataset.yaml", content);
+            // TODO: Add support for multiple annotation definitions
+            AnnotationDefinition annotationDefinition = DeserializeObjectFromFile<AnnotationDefinitions>(Path.Combine(soloPath, "annotation_definitions.json")).Values[0];
+            GenerateDatasetYaml(yoloPath, annotationDefinition);
         }
 
+        // TODO: Add validations
+        /// <summary>
+        /// Deserializes a JSON file into an object.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize.</typeparam>
+        /// <param name="filePath">The path to the JSON file.</param>
+        /// <returns>The deserialized object.</returns>
+        private static T DeserializeObjectFromFile<T>(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"Could not find {Path.GetFileName(filePath)}.");
+
+            string data = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        /// <summary>
+        /// Validates and sanitizes a file or directory path.
+        /// </summary>
+        /// <param name="path">The path to validate and sanitize.</param>
         private static void ValidateAndSanitizePath(ref string path)
         {
             if (string.IsNullOrWhiteSpace(path))
                 throw new InvalidPathException("Path cannot be null or empty.");
 
-            path = path.Replace('\\', '/').Trim();
+            path = path.Trim();
 
             if (Path.GetInvalidPathChars().Any(path.Contains))
                 throw new InvalidPathException("Path contains invalid characters.");
@@ -134,42 +118,95 @@ namespace z3lx.solo2yolo
                 throw new InvalidPathException("Path does not exist.");
         }
 
-        private static (string, string, string) CreateYoloDirectory(string path)
+        /// <summary>
+        /// Creates a YOLO directory for storing the converted dataset.
+        /// </summary>
+        /// <param name="path">The base path to create the YOLO directory.</param>
+        /// <returns>The path to the created YOLO directory.</returns>
+        private static string CreateYoloDirectory(string path)
         {
             int i = 0;
-            string newYoloDirPath = $"{path}/yolo";
+            string yoloPath = Path.Combine(path, "yolo");
 
-            while (Directory.Exists(newYoloDirPath))
+            while (Directory.Exists(yoloPath))
             {
                 i++;
-                newYoloDirPath = $"{path}/yolo_{i}";
+                yoloPath = Path.Combine(path, $"yolo_{i}");
             }
 
-            string parent = $"{newYoloDirPath}";
-            string images = $"{newYoloDirPath}/images";
-            string labels = $"{newYoloDirPath}/labels";
-            Directory.CreateDirectory(newYoloDirPath);
-            Directory.CreateDirectory(images);
-            Directory.CreateDirectory(labels);
-            return (parent, images, labels);
+            Directory.CreateDirectory(yoloPath);
+            Directory.CreateDirectory(Path.Combine(yoloPath, "images"));
+            Directory.CreateDirectory(Path.Combine(yoloPath, "labels"));
+            return yoloPath;
         }
 
-        private static Metadata GetMetadata(string soloPath)
+        // Can there be multiple of the same annotation type?
+        // TODO: Add support for other computer vision tasks
+        /// <summary>
+        /// Converts the labeling data based on the computer vision task.
+        /// </summary>
+        /// <param name="capture">The capture to convert.</param>
+        /// <param name="task">The computer vision task.</param>
+        /// <returns>The converted labeling data.</returns>
+        private static string ConvertLabelingData(RgbCapture capture, ComputerVisionTask task)
         {
-            string metadataPath = $"{soloPath}/metadata.json";
-            if (!File.Exists(metadataPath))
-                throw new FileNotFoundException("Could not find metadata.json");
-            string metadataContent = File.ReadAllText(metadataPath);
-            return JsonConvert.DeserializeObject<Metadata>(metadataContent);
+            string labelingData = string.Empty;
+
+            switch (task)
+            {
+                case ComputerVisionTask.Classify:
+                    throw new NotImplementedException();
+
+                case ComputerVisionTask.Detect:
+                    BoundingBox2DAnnotation annotation = capture.Annotations.OfType<BoundingBox2DAnnotation>().FirstOrDefault();
+                    if (annotation == null)
+                        return null;
+
+                    foreach (BoundingBox2DAnnotation.Value value in annotation.Values)
+                    {
+                        int objectClassId = value.LabelId;
+                        double x = (double)(value.Origin.x + (value.Dimension.x / 2)) / capture.Dimension.x;
+                        double y = (double)(value.Origin.y + (value.Dimension.y / 2)) / capture.Dimension.y;
+                        double width = (double)value.Dimension.x / capture.Dimension.x;
+                        double height = (double)value.Dimension.y / capture.Dimension.y;
+                        labelingData += $"{objectClassId} {x} {y} {width} {height}\n";
+                    }
+                    break;
+
+                case ComputerVisionTask.Segment:
+                    throw new NotImplementedException();
+
+                case ComputerVisionTask.Pose:
+                    throw new NotImplementedException();
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(task), task, "Unsupported task type.");
+            }
+
+            return labelingData;
         }
 
-        private static AnnotationDefinitions GetAnnotationDefinition(string soloPath)
+        /// <summary>
+        /// Generates the dataset YAML file for YOLO.
+        /// </summary>
+        /// <param name="yoloPath">The path to the YOLO directory.</param>
+        /// <param name="annotationDefinition">The annotation definition.</param>
+        private static void GenerateDatasetYaml(string yoloPath, AnnotationDefinition annotationDefinition)
         {
-            string annotationDefinitionPath = $"{soloPath}/annotation_definitions.json";
-            if (!File.Exists(annotationDefinitionPath))
-                throw new FileNotFoundException("Could not find annotation_definitions.json");
-            string annotationDefinitionContent = File.ReadAllText(annotationDefinitionPath);
-            return JsonConvert.DeserializeObject<AnnotationDefinitions>(annotationDefinitionContent);
+            string content =
+                $"path: {yoloPath} # dataset root dir\n" +
+                "train: images # train images (relative to 'path')\n" +
+                "val: images # val images (relative to 'path')\n" +
+                "test: # test images (optional)\n" +
+                "\n" +
+                "# Classes\n" +
+                "names:\n";
+
+            foreach (AnnotationDefinition.Specification spec in annotationDefinition.Specifications)
+                content += $"  {spec.LabelId}: {spec.labelName}\n";
+
+            string yamlFilePath = Path.Combine(yoloPath, "dataset.yaml");
+            File.WriteAllText(yamlFilePath, content);
         }
     }
 }
