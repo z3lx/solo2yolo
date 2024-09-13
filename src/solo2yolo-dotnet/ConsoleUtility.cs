7cac626ec5f5ddc00969bc4e3233e000e249ca7e
z3lx <57017122+z3lx@users.noreply.github.com>
2024-09-13 08:53:08 -0400

    Add logging capabilities and improve unity integration in converter

---
+++ src/solo2yolo-dotnet/ConsoleUtility.cs
@@ -0,0 +1,26 @@
+﻿namespace z3lx.solo2yolo
+{
+    public static class ConsoleUtility
+    {
+        public static void PrintInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"[INFO] {message}");
+            Console.ResetColor();
+        }
+
+        public static void PrintWarning(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[WARN] {message}");
+            Console.ResetColor();
+        }
+
+        public static void PrintError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[ERR ] {message}");
+            Console.ResetColor();
+        }
+    }
+}
